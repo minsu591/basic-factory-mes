7724597d397e99d0a46b4885f1e5a6d1247a4b2c
MinSeo Kim <minsu591@naver.com>
2022-08-12 12:12:51 +0900

    개발환경 세팅

---
+++ basicFactory/src/test/java/com/mes/bf/BasicFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.bf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
