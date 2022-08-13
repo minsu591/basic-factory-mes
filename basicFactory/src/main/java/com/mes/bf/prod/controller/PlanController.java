3f222270d15571557767efcf1d0d4ce353fe82cd
jonghyuk <kasiruske@naver.com>
2022-08-13 14:40:57 +0900

    add layout

--- basicFactory/src/main/java/com/mes/bf/prod/controller/PlanController.java
+++ basicFactory/src/main/java/com/mes/bf/prod/controller/PlanController.java
@@ -1,5 +1,18 @@
 package com.mes.bf.prod.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class PlanController {
-//test
+	
+	@RequestMapping("/content")
+	public String test() {
+		return "prod/content";
+	}
+	
+	@RequestMapping("/prod")
+	public String test2() {
+		return "prod/prod";
+	}
 }
