b267bca6a9a3b4ebd44fadd5cf2ef0f41b477997
jonghyuk <kasiruske@naver.com>
2022-08-13 23:25:10 +0900

    jh add instruction page

--- basicFactory/src/test/java/com/mes/bf/BasicFactoryApplicationTests.java
+++ basicFactory/src/test/java/com/mes/bf/BasicFactoryApplicationTests.java
@@ -1,7 +1,6 @@
 package com.mes.bf;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mes.bf.prod.service.InstructionService;
