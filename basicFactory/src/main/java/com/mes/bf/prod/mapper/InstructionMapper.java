495c814a7d6278719d031ca691f88a6da2cfe33a
jonghyuk <kasiruske@naver.com>
2022-08-13 19:13:27 +0900

    add mapper Test

--- basicFactory/src/main/java/com/mes/bf/prod/mapper/InstructionMapper.java
+++ basicFactory/src/main/java/com/mes/bf/prod/mapper/InstructionMapper.java
@@ -1,5 +1,13 @@
 package com.mes.bf.prod.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mes.bf.prod.vo.InstructionVO;
+
+@Mapper
 public interface InstructionMapper {
 
+	public InstructionVO getInst();
+	
+	
 }
