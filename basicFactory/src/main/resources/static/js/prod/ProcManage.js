8965c6b76afd1817aeafe378ec42c3cb22430d05
jonghyuk <kasiruske@naver.com>
2022-08-19 03:56:50 +0900

    commit

--- basicFactory/src/main/resources/static/js/prod/ProcManage.js
+++ basicFactory/src/main/resources/static/js/prod/ProcManage.js
@@ -29,6 +29,35 @@ $(document).ready(function () {
     });
   }
 
+  //제품검색버튼 이벤트
+  $("#findProductbtn").click(function () {
+    let code = $("#prdCdCode").val();
+    let name = $("#prdCdName").val();
+    console.log("code -> " + code + " name->" + name);
+
+    $.ajax({
+      url: `getProduct`,
+      method: "GET",
+      contentType: "application/json;charset=utf-8",
+      dataType: "json",
+      data: {
+        prdCdCode: code,
+        prdCdName: name,
+      },
+      error: function (error, status, msg) {
+        alert("상태코드 " + status + "에러메시지" + msg);
+      },
+      success: function (data) {
+        console.log(data);
+
+        let index = 0;
+        index += 1;
+        $("#findProducttbody tr").remove();
+        makeRow(data, index);
+      },
+    });
+  });
+
   //모달 테이블 클릭 이벤트
   $("#findProductTable").on("click", "tr", function () {
     console.log($(this).find("td:eq(2)").text());
