b4d3b431fc2ad1fe15186184f871614b8f6ad86e
miraeryu <alfo678@gmail.com>
2022-08-21 05:27:05 +0900

    add RSCcontroller

--- basicFactory/src/main/java/com/mes/bf/rsc/mapper/RscMapper.java
+++ basicFactory/src/main/java/com/mes/bf/rsc/mapper/RscMapper.java
@@ -1,5 +1,15 @@
 package com.mes.bf.rsc.mapper;
 
-public class RscMapper {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mes.bf.rsc.vo.RscStockVO;
+
+@Mapper
+public interface RscMapper {
+
+
+	//재고
+	List<RscStockVO> StockList();
 }
