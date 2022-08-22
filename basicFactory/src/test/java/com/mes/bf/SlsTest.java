25a13ce055f673199211f9bba99773ddb9e517a4
ImkyeongJeong <imkyeongjeong@gmail.com>
2022-08-22 22:33:12 +0900

    Add ProductStock

--- basicFactory/src/test/java/com/mes/bf/SlsTest.java
+++ basicFactory/src/test/java/com/mes/bf/SlsTest.java
@@ -7,14 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.mes.bf.sales.service.SlsOrdService;
+import com.mes.bf.sales.service.SlsStockService;
 import com.mes.bf.sales.vo.SlsOrdHdDtlVO;
+import com.mes.bf.sales.vo.SlsStockVO;
 
 @SpringBootTest
 public class SlsTest {
 	
 	@Autowired SlsOrdService service;
+	@Autowired SlsStockService stockService;
 	
-	@Test
+	//@Test
 	void findAllOrd() {
 		List<SlsOrdHdDtlVO> list1 = service.findAllOrder();
 		for(int i = 0; i < list1.size(); i++) {
@@ -27,6 +30,14 @@ public class SlsTest {
 		List<SlsOrdHdDtlVO> list1 = service.findOrder("2022-08-18","2022-08-18","예담");
 		for(int i = 0; i < list1.size(); i++) {
 			System.out.println(list1.get(i).toString());
+		}
+	}
+	
+	@Test
+	void finAllStock() {
+		List<SlsStockVO> list = stockService.findAllStock();
+		for(int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i).toString());
 		}
 	}
 }
