bf787f1fb11e84003fb5a9a19bc9691f54ecebea
woo yw <wyw604@daum.net>
2022-08-23 09:06:41 +0900

    설비 조회

--- basicFactory/src/main/java/com/mes/bf/eqp/mapper/MchnMapper.java
+++ basicFactory/src/main/java/com/mes/bf/eqp/mapper/MchnMapper.java
@@ -1,5 +1,19 @@
 package com.mes.bf.eqp.mapper;
 
-public class EqpMapper {
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.mes.bf.eqp.vo.MchnVO;
+
+@Mapper
+public interface MchnMapper {
+	
+	//설비 조회
+	List<MchnVO> MchnAllList();
+	
+	//설비 코드별 조회
+	List<MchnVO> findMchn(Map<String, Object> params);
 
 }
--- basicFactory/src/main/java/com/mes/bf/eqp/service/MchnService.java
+++ basicFactory/src/main/java/com/mes/bf/eqp/service/MchnService.java
@@ -1,5 +1,16 @@
 package com.mes.bf.eqp.service;
 
-public class EqpService {
+import java.util.List;
+import java.util.Map;
+
+import com.mes.bf.eqp.vo.MchnVO;
+
+public interface MchnService {
+	
+	//설비 조회
+	List<MchnVO> MchnAllList();
+	
+	//설비 코드별 조회
+	List<MchnVO> findMchn(Map<String, Object> params);
 
 }
