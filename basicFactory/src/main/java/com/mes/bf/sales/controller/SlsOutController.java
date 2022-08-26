ffba8fa64bad66bd8e2f10ece22ac9c8b78d22a6
ImkyeongJeong <imkyeongjeong@gmail.com>
2022-08-26 17:44:04 +0900

    Add SlsOut

--- basicFactory/src/main/java/com/mes/bf/sales/controller/SlsOutController.java
+++ basicFactory/src/main/java/com/mes/bf/sales/controller/SlsOutController.java
@@ -4,18 +4,17 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.mes.bf.cmn.vo.VendorCodeVO;
 import com.mes.bf.prod.service.InstructionService;
 import com.mes.bf.sales.service.SlsOutService;
+import com.mes.bf.sales.vo.SlsOrdHdDtlVO;
+import com.mes.bf.sales.vo.SlsOrdHdVO;
+import com.mes.bf.sales.vo.SlsOutDtlVO;
 import com.mes.bf.sales.vo.SlsOutHdDtlVO;
 
 @RestController
@@ -41,11 +40,37 @@ public class SlsOutController {
 	
 	//완제품 출고내역 단건 조회
 	@GetMapping(value = "/findOut")
-	public List<SlsOutHdDtlVO> finOut(@RequestParam Map<String, String> param){
+	public List<SlsOutHdDtlVO> findOut(@RequestParam Map<String, String> param){
 		List<SlsOutHdDtlVO> list = service.findOut(param.get("outSdate"),
 				 								   param.get("outEdate"),
 				 								   param.get("vendorName"));
 		return list;
 	}
+	
+	//완제품 출고관리 페이지 이동
+	@RequestMapping("/outManage")
+	public ModelAndView outManage() {
+		ModelAndView mav = new ModelAndView("sales/outManage");
+		return mav;
+	}
+	
+	//완제품 출고관리에서 미출고된 주문내역 조회 모달창
+	@RequestMapping("/NotOutOrderView")
+	public List<SlsOrdHdDtlVO> findNotOutOrder(@RequestParam Map<String, String> param){
+		System.out.println("접속!!!");
+		List<SlsOrdHdDtlVO> list = service.findNotOut(param.get("ordSdate"),
+														   param.get("ordEdate"));
+		return list;
+	}
+	
+	//출고관리에서 미출고된 주문내역 상세조회
+	@RequestMapping("/NotOutOrderView/dtl")
+	public List<SlsOutDtlVO> findNotOutDtl(@RequestParam Map<String, String> param){
+		List<SlsOutDtlVO> list = service.findNotOutDtl(param.get("slsOrdHdNo"));
+		return list;
+	}
+	
+	//출고관리에서 출고내역 조회 모달
+	//출고관리에서 출고내역 상세 조회
 	
 }
