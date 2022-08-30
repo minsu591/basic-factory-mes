31fb34592f467c39194d3a3b028fcb9ef378af0b
woo yw <wyw604@daum.net>
2022-08-30 19:26:31 +0900

    불량코드 모달창 불량처리관리 구현중

--- basicFactory/src/main/java/com/mes/bf/prod/service/FltyPrcsService.java
+++ basicFactory/src/main/java/com/mes/bf/prod/service/FltyPrcsService.java
@@ -2,11 +2,17 @@ package com.mes.bf.prod.service;
 
 import java.util.List;
 
+import com.mes.bf.cmn.vo.FaultyCodeVO;
 import com.mes.bf.prod.vo.FltyPrcsVO;
 
 public interface FltyPrcsService {
 	
-	//불량처리
+	//불량처리목록
+	List<FltyPrcsVO> findlistFltyPrcs(String fltyPrcsSdate, String fltyPrcsEdate);
+	//생산중불량조회
+	List<FltyPrcsVO> findProcFlty();
+	//불량코드 조회
+	List<FaultyCodeVO> findFltyCode(String faultyCode);
 	
 	
 	//불량처리조회
