f77b3ebb185da15b47ce58351f042626e3417163
jonghyuk <kasiruske@naver.com>
2022-09-11 18:33:46 +0900

    jonghyuk add remark

--- basicFactory/src/main/java/com/mes/bf/common/Criteria.java
+++ basicFactory/src/main/java/com/mes/bf/common/Criteria.java
@@ -7,8 +7,8 @@ public class Criteria {
 	private int pageNum = 1; // 현재페이지
 	private int amount = 10; // 한페이지에 출력할 게시글 수
 
-	private String type;
-	private String keyword;
+	private String type; //검색 분류 
+	private String keyword; // 검색어
 	private String keyword2;
 
 	public String[] getTypeArr() {
