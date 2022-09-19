d2ebf7807ebcedfaf0626b0a7d3ce1b666ed2170
MinSeo Kim <minsu591@naver.com>
2022-09-19 11:08:32 +0900

    modify

--- basicFactory/src/main/java/com/mes/bf/cmn/interceptor/WebMvmConfig.java
+++ basicFactory/src/main/java/com/mes/bf/cmn/interceptor/WebMvmConfig.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvmConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new LoginInterceptor())
-				.addPathPatterns("/prod/**","/cmn/**","/eqp/**","/rsc/**","/sls/**")
-				.excludePathPatterns("/cmn/login","/cmn/login/check","/cmn/login/reset","/cmn/login/reset/**");
-		//		.excludePathPatterns("/css/**","/fonts/**","/plugin/**","/scripts/**");
-	}
+//	@Override
+//	public void addInterceptors(InterceptorRegistry registry) {
+//		registry.addInterceptor(new LoginInterceptor())
+//				.addPathPatterns("/prod/**","/cmn/**","/eqp/**","/rsc/**","/sls/**")
+//				.excludePathPatterns("/cmn/login","/cmn/login/check","/cmn/login/reset","/cmn/login/reset/**");
+//		//		.excludePathPatterns("/css/**","/fonts/**","/plugin/**","/scripts/**");
+//	}
 }
