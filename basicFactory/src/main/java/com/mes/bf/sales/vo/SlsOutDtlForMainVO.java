930570ee374cde194a29a7288f9f426226c2a494
MinSeo Kim <minsu591@naver.com>
2022-09-20 12:03:15 +0900

    make main

--- basicFactory/src/main/java/com/mes/bf/sales/vo/SlsOutDtlForMainVO.java
+++ basicFactory/src/main/java/com/mes/bf/sales/vo/SlsOutDtlForMainVO.java
@@ -6,9 +6,8 @@ import lombok.Data;
 
 @Data
 public class SlsOutDtlForMainVO {
-	
+	private Date slsOutHdDate;          //hd 데이트
 	private String finPrdCdCode;		//완제품코드
 	private String finPrdCdName;		//제품명
 	private Integer slsOutDtlVol;		//출고량
-	private Integer rank;
 }
